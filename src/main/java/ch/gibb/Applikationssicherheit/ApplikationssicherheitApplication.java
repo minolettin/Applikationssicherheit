874acb0919650b9604ec043b4b7fb5bdf230b99b
Nathanael Minoletti <nmi106019@iet-gibb.ch>
2021-05-26 09:24:34 +0200

    Initial commit

---
+++ src/main/java/ch/gibb/Applikationssicherheit/ApplikationssicherheitApplication.java
@@ -0,0 +1,13 @@
+package ch.gibb.Applikationssicherheit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplikationssicherheitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplikationssicherheitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/gibb/Applikationssicherheit/ApplikationssicherheitApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.gibb.Applikationssicherheit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplikationssicherheitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
