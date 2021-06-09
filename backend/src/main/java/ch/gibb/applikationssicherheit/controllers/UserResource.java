9f238738f76fb357e58a2702863199bd07f763d6
Nathanael Minoletti <nmi106019@iet-gibb.ch>
2021-06-09 09:33:21 +0200

    added request mappings

--- backend/src/main/java/ch/gibb/applikationssicherheit/controllers/UserResource.java
+++ backend/src/main/java/ch/gibb/applikationssicherheit/controllers/UserResource.java
@@ -1,11 +1,24 @@
 package ch.gibb.applikationssicherheit.controllers;
 
+import ch.gibb.applikationssicherheit.service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class UserResource {
 
+    @Autowired
+    ResourceService resourceService;
 
+    @GetMapping({"/executecommand"})
+    public String executeCommand(@RequestParam(name = "sysopt") String command) {
+        return resourceService.executeCommand(command);
+    }
+
+    @PostMapping({"/register"})
+    public String register(@RequestParam String username, String password) {
+        return resourceService.register(username, password);
+    }
 
 }
