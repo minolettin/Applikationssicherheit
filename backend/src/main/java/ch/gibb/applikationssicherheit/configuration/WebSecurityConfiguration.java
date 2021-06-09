2d495bbe736d753f4785aeadd1cc4d577137ceec
Aaron Beer <abe106005@iet-gibb.ch>
2021-06-09 09:53:36 +0200

    enabled csrf and called pnpm install

--- backend/src/main/java/ch/gibb/applikationssicherheit/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/ch/gibb/applikationssicherheit/configuration/WebSecurityConfiguration.java
@@ -31,8 +31,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
+        httpSecurity.csrf().and()
                 .authorizeRequests().antMatchers("/helloadmin")
                 .hasRole("ADMIN")
                 .antMatchers("/hellouser")
